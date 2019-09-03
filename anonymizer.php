abd08d93802bc008d6134aa8cde0150f7808488a
Tomas Kovarik <info@rs-kovarik.cz>
2019-09-03 21:15:20 +0200

    PHP script

---
+++ anonymizer.php
@@ -0,0 +1,16 @@
+<?php
+
+$files = glob('./pre/*.{jpg,png,gif}', GLOB_BRACE);
+foreach($files as $file) {
+$path      = $file;     
+$result_path  = $file;
+$image        = imagecreatetruecolor(500, 700);
+$image        = imagecreatefrompng($path);
+$color        = imagecolorallocatealpha($image, 255, 255, 255, 0);
+imagefilledrectangle($image, 380, 120,  600, 180, $color);
+imagefilledrectangle($image, 0, 85,  800, 120, $color);
+header('Content-Type: image/png');
+imagepng($image, $result_path);
+imagedestroy($image);
+}
+
